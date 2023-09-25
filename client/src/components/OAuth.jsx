82d04a8a1013b6617f3e7435d058960e8a6a0665
Yash Deshmukh <yashdeshmukh300@gmail.com>
2023-09-25 09:45:36 +0530

    complete update user functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,30 +2,34 @@ import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
- const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
+
       const result = await signInWithPopup(auth, provider);
-     const res = await fetch("/api/auth/google", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-       name: result.user.displayName,
-       email: result.user.email,
-       photo: result.user.photoURL,
-      }),
-    });
-    const data = await res.json();
-    dispatch(signInSuccess(data));
-   }
-     catch (error) {
-      console.log("could not sign in with google", error);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      console.log("could not login with google", error);
     }
   };
   return (
@@ -34,7 +38,7 @@ export default function OAuth() {
       onClick={handleGoogleClick}
       className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95"
     >
-      Continue with Google
+      Continue with google
     </button>
   );
 }
